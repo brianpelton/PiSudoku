5305d0146b0c9f5e7d6e258c44e74f48ff306ab9
Brian Pelton <brian.pelton@gmail.com>
2022-03-15 20:41:15 -0700

    Button box can toggle buttons.

--- PiSudoku/PiSudoku/ButtonBox.Designer.cs
+++ PiSudoku/PiSudoku/ButtonBox.Designer.cs
@@ -50,6 +50,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(31, 31);
             this.button1.TabIndex = 0;
+            this.button1.Tag = "3";
             this.button1.Text = "3";
             this.button1.UseVisualStyleBackColor = true;
             // 
@@ -61,6 +62,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(31, 31);
             this.button2.TabIndex = 1;
+            this.button2.Tag = "1";
             this.button2.Text = "1";
             this.button2.UseVisualStyleBackColor = true;
             // 
@@ -72,6 +74,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(31, 31);
             this.button3.TabIndex = 2;
+            this.button3.Tag = "4";
             this.button3.Text = "4";
             this.button3.UseVisualStyleBackColor = true;
             // 
@@ -83,6 +86,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(31, 31);
             this.button4.TabIndex = 3;
+            this.button4.Tag = "1";
             this.button4.Text = "1";
             this.button4.UseVisualStyleBackColor = true;
             // 
@@ -94,6 +98,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(31, 31);
             this.button5.TabIndex = 4;
+            this.button5.Tag = "5";
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
             // 
@@ -105,6 +110,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(31, 31);
             this.button6.TabIndex = 5;
+            this.button6.Tag = "9";
             this.button6.Text = "9";
             this.button6.UseVisualStyleBackColor = true;
             // 
@@ -116,6 +122,7 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(31, 31);
             this.button7.TabIndex = 6;
+            this.button7.Tag = "2";
             this.button7.Text = "2";
             this.button7.UseVisualStyleBackColor = true;
             // 
@@ -127,6 +134,7 @@
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(31, 31);
             this.button8.TabIndex = 7;
+            this.button8.Tag = "6";
             this.button8.Text = "6";
             this.button8.UseVisualStyleBackColor = true;
             // 
@@ -138,6 +146,7 @@
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(31, 31);
             this.button9.TabIndex = 8;
+            this.button9.Tag = "5";
             this.button9.Text = "5";
             this.button9.UseVisualStyleBackColor = true;
             // 
@@ -149,6 +158,7 @@
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(31, 31);
             this.button10.TabIndex = 9;
+            this.button10.Tag = "3";
             this.button10.Text = "3";
             this.button10.UseVisualStyleBackColor = true;
             // 
@@ -160,6 +170,7 @@
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(31, 31);
             this.button11.TabIndex = 10;
+            this.button11.Tag = "5";
             this.button11.Text = "5";
             this.button11.UseVisualStyleBackColor = true;
             // 
@@ -171,6 +182,7 @@
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(31, 31);
             this.button12.TabIndex = 11;
+            this.button12.Tag = "8";
             this.button12.Text = "8";
             this.button12.UseVisualStyleBackColor = true;
             // 
--- PiSudoku/PiSudoku/ButtonBox.cs
+++ PiSudoku/PiSudoku/ButtonBox.cs
@@ -5,6 +5,45 @@
         public ButtonBox()
         {
             InitializeComponent();
+
+            if (DesignMode)
+                return;
+
+            button1.Click += ButtonClick;
+            button2.Click += ButtonClick;
+            button3.Click += ButtonClick;
+            button4.Click += ButtonClick;
+            button5.Click += ButtonClick;
+            button6.Click += ButtonClick;
+            button7.Click += ButtonClick;
+            button8.Click += ButtonClick;
+            button9.Click += ButtonClick;
+            button10.Click += ButtonClick;
+            button11.Click += ButtonClick;
+            button12.Click += ButtonClick;
+        }
+
+        private void ButtonClick(object? sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            var currentColor = button.BackColor;
+
+            bool isEnabled = !string.IsNullOrEmpty(button.Text);
+
+            if (isEnabled)
+            {
+                button.Text = string.Empty;
+                button.FlatStyle = FlatStyle.Flat;
+            }
+            else
+            {
+                button.Text = button.Tag.ToString();
+                button.FlatStyle = FlatStyle.Standard;
+            }
+
+            //var newColor = Color.FromArgb(currentColor.A == 255 ? 0 : 255, currentColor.R, currentColor.G, currentColor.B);
+            //button.BackColor = newColor;
         }
 
         public Color BackColor
@@ -17,6 +56,16 @@
             {
                 base.BackColor = value;
             }
+        }
+
+        public int[] PossibleValues { get; set; }
+
+        public int Value { get; set; }
+
+        public void Update()
+        {
+
+
         }
     }
 }

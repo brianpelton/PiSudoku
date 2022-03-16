b17a78870b52a5ee8fd2ab18041b94083409a540
Brian Pelton <brian.pelton@gmail.com>
2022-03-15 17:37:24 -0700

    Generate 12x12 grid of text boxes.

--- PiSudoku/PiSudoku/MainForm.Designer.cs
+++ PiSudoku/PiSudoku/MainForm.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpBoard = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.grpBoard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBoard
+            // 
+            this.grpBoard.Controls.Add(this.textBox1);
+            this.grpBoard.Location = new System.Drawing.Point(12, 12);
+            this.grpBoard.Name = "grpBoard";
+            this.grpBoard.Size = new System.Drawing.Size(526, 333);
+            this.grpBoard.TabIndex = 0;
+            this.grpBoard.TabStop = false;
+            this.grpBoard.Text = "Board";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(6, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(26, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grpBoard);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.grpBoard.ResumeLayout(false);
+            this.grpBoard.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GroupBox grpBoard;
+        private TextBox textBox1;
     }
 }
--- PiSudoku/PiSudoku/MainForm.cs
+++ PiSudoku/PiSudoku/MainForm.cs
@@ -6,5 +6,39 @@ namespace PiSudoku
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            CreateTextBoxes();
+        }
+
+        private void CreateTextBoxes()
+        {
+            var lastTexbox = textBox1;
+
+            for (int i = 1; i < 144; i++)
+            {
+                var tb = new TextBox();
+
+                tb.Name = $"Square{i}";
+                tb.Parent = grpBoard;
+                tb.Font = lastTexbox.Font;
+                tb.Width = lastTexbox.Width;
+                tb.Height = lastTexbox.Height;
+                tb.Left = lastTexbox.Left + tb.Width + 5;
+                tb.Top = lastTexbox.Top;
+
+                if (i % 12 == 0)
+                {
+                    tb.Top += tb.Height + 5;
+                    tb.Left = textBox1.Left;
+                }
+
+                lastTexbox = tb;
+            }
+
+            grpBoard.Width = lastTexbox.Left + lastTexbox.Width + 5;
+            grpBoard.Height = lastTexbox.Top + lastTexbox.Height + 5;
+        }
     }
 }
